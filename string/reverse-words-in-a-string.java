c392eb62a92de5b509a650c48c9434993f653291
Shloka Mahesheka <107931497+shlokam@users.noreply.github.com>
2024-03-19 23:32:08 +0530

    :ballot_box_with_check: reverse-words-in-a-string [Runtime: 12 ms; Memory: 45.3 MB]

--- string/reverse-words-in-a-string.java
+++ string/reverse-words-in-a-string.java
@@ -1,19 +1,24 @@
 class Solution {
     public String reverseWords(String s) {
         String[] sArr = s.split(" ");
-        String[] sArrWithoutSpace = new String[sArr];
+        String[] sArrWithoutSpace = new String[sArr.length];
+        String res="";
 
         int j=0;
         for(int i=0;i<sArr.length;++i){
-            System.out.println(i + " " + sArr[i]);
-            if(!sArr[i].equals(" ")){
+            if(!sArr[i].equals("")){
                 sArrWithoutSpace[j] = sArr[i];
                 ++j;
             }
         }
+        int num = j;
+        for(int i=num-1;i>=0;--i){
+             if(res.equals(""))
+                res =  sArrWithoutSpace[i];
+            else
+                res = res + " " + sArrWithoutSpace[i];
+        }
 
-        
-
-        return s;
+        return res;
     }
 }
