dc5c21f1641d70c3a250abbd2176e66e54a2ea2f
Shloka Mahesheka <107931497+shlokam@users.noreply.github.com>
2024-03-21 23:51:58 +0530

    :ballot_box_with_check: palindrome-number [Runtime: 6 ms; Memory: 44.5 MB]

---
+++ math/palindrome-number.java
@@ -0,0 +1,25 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        
+        if(x<0)
+            return false;
+        List<Integer> list = new ArrayList<>();
+
+        while(x>0){
+            list.add(x%10);
+            x = x/10;
+        }
+
+        int i=0, j = list.size()-1;
+
+        while(i<j){
+            if(list.get(i) != list.get(j))
+                return false;
+            
+            ++i;
+            --j;
+        }
+
+        return true;
+    }
+}
