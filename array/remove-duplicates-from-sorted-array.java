3a1578386b19ebe8d3fe2de97d47516958b1bff0
Shloka Mahesheka <107931497+shlokam@users.noreply.github.com>
2024-07-03 19:01:25 +0530

    :ballot_box_with_check: remove-duplicates-from-sorted-array [Runtime: 0 ms; Memory: 44.7 MB]

---
+++ array/remove-duplicates-from-sorted-array.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        int n = nums.length;
+
+        if(n==1)
+            return 1;
+
+        else if(n==2){
+
+        }
+
+        int i=0 , j =1;
+
+        for(;j<n;){
+            if(nums[i] == nums[j])
+                ++j;
+            else{
+                nums[++i] = nums[j];    
+                ++j;
+            }
+        }
+
+        return i+1;
+    }
+}
