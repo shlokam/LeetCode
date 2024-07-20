e7764cc2fe664e691027fc8ccd059ebc247186ce
Shloka Mahesheka <107931497+shlokam@users.noreply.github.com>
2024-07-20 18:15:09 +0530

    :ballot_box_with_check: two-sum [Runtime: 16 ms; Memory: 45.5 MB]

--- array/two-sum.java
+++ array/two-sum.java
@@ -1,43 +1,19 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        Map<Integer,Integer> count = new HashMap<>();
 
-        for(int i=0;i<nums.length;++i){
-            if(count.containsKey(nums[i]))
-                count.put(nums[i],count.get(nums[i])+1);
-            else
-                count.put(nums[i],1);
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            map.put(nums[i], i);
         }
 
-   
-        int ans1=0,ans2=0,ele,ind1,ind2;
-        ind1 = -1; ind2 = -1;
+        System.out.println(map); // 3 -> 1
 
-        for(int i=0;i<nums.length;++i){
-            ele = target - nums[i];
-            if(count.containsKey(ele)){
-                if(ele != nums[i]){
-                    ans1 = nums[i];
-                    ans2 = ele;
-                    break;
-                }else if(count.get(nums[i])>1){
-                    ans1 = nums[i];
-                    ans2 = nums[i];
-                    break;
-                }
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i]; // 3
+            if (map.containsKey(complement) && map.get(complement) != i) {
+                return new int[] { i, map.get(complement) };
             }
         }
-
-        for(int i=0;i<nums.length;++i){
-            if(ind1 ==-1 && ans1 == nums[i])
-                ind1 = i;
-            else if(ind2 ==-1 && ans2 == nums[i])
-                ind2 = i;
-
-            if(ind1 != -1 && ind2 != -1)
-                break;
-        }
-
-        return new int[]{ind1,ind2};
+        return null;
     }
 }
